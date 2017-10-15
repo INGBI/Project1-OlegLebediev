f33b32318294d8fd3de133c740097684ea45370c
Oleg Lebediev <lebedyevoleg@gmail.com>
2017-10-15 14:29:43 -0400

    Just added some constants and copied the Compiler info from the Slack

---
+++ src/main/scala/edu/towson/cis/cosc455/olebed1/project1/CONSTANTS.scala
@@ -0,0 +1,26 @@
+package edu.towson.cis.cosc455.olebed1.project1
+
+/**
+  * Created by olebed1 on 10/15/2017.
+  */
+object CONSTANTS {
+    val DOCB : String = 	"\\BEGIN"
+    val DOCE : String = 	"\\END"
+    val TITLEB : String = "\\TITLE["
+    val BRACKETE : String = "]"
+    val HEADING : String ="#"
+    val PARAB : String ="\\PARAB"
+    val PARAE : String ="\\PARAE"
+    val BOLD : String ="*"
+    val LISTITEM : String ="+"
+    val NEWLINE : String = "\\"
+    val LINKB : String ="["
+    val ADDRESSB : String ="("
+    val ADDRESSE : String =")"
+    val IMAGEB : String ="!["
+    val DEFB : String ="\\DEF["
+    val EQSIGN : String ="="
+    val USEB : String ="\\USE["
+    val REQTEXT : String = TEXT
+    val TEXT : String = _
+}
